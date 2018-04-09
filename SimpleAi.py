9d8333259a8ce5269e6925d89c0542acdab3f97d
qwe81130446 <xx6659@gmail.com>
2018-04-09 19:21:20 -0400

    SimpleAi completed

--- SimpleAi.py
+++ SimpleAi.py
@@ -1,27 +1,24 @@
 import random
+from itertools import *
 #game of sticks using probability basic idea of AI
 
 
 class Player():
-	def __init__(self, sticks):
-		self.array = None
+	def __init__(self):
 		self.name = None
-		self.sticks = sticks
-	def pickSticks(self, pick):
-		self.sticks -= pick
 
 class Ai(Player):
-	def __init__(self, sticks):
-		super(Ai, self).__init__(sticks)
+	def __init__(self):
+		super(Ai, self).__init__()
 		self.name = ' '.join([chr(random.randrange(100)) for x in range(4)])
-		self.array = [0] * sticks
+		self.array = {}
+		
 
 
 class GameofSticks():
 	def __init__(self):
-		self.numSticks = 20
-		self.name = input(str("Enter in your name: "))
-		print("Hello", self.name)
+		self.startSticks = 20
+		self.numSticks = self.startSticks
 		self.welcome()
 		self.chooseOption()
 
@@ -40,21 +37,98 @@ class GameofSticks():
 		if self.option == 1:
 			self.pvp()
 		elif self.option == 2:
-			self.train()
+			value = 100
+			while True:
+				try:
+					value = int(input("Round should Ai play against each other? "))
+					if(value < 1):
+						print("enter valid integer..")
+						continue
+					break
+				except ValueError as e:
+					print("enter valid integer..")
+
+
+			self.train(value)
 		else:
 			exit()
-	def winner(self, players):
-		pass
+	def winner(self, player):
+		if self.numSticks <= 0:
+			return True
+		return False
 
 	def pvp(self):
-		player1 = Player(self.numSticks)
-		player2 = Player(self.numSticks)
-		player1.name = str(input("what is player 1 name? "))
-		player2.name = str(input("what is player 2 name? "))
+		player1 = Player()
+		player2 = Player()
+		setattr(player1, "name", str(input("what is player 1 name? ")))
+		setattr(player2, "name", str(input("what is player 2 name? ")))
 		players = [player1, player2]
-		players = random.shuffle(players)
-		print(players[0].name + "is going first")
+		random.shuffle(players)
+		print(getattr(players[0], "name") + " is going first")
 
+
+		gen = cycle(players)
+		currentPlayer = next(gen)
+		while self.winner(currentPlayer) == False:
+			try:
+				picks = int(input("How many sticks you want to pick? "))
+				self.numSticks -= picks
+				print("{} picked {} sticks, There are {} sticks left..".format(getattr(currentPlayer, "name"), picks ,self.numSticks))
+				currentPlayer = next(gen)
+			except ValueError as e:
+				print("Please choose between 1-3 sticks")
+
+		print(getattr(currentPlayer, "name") + " won !!")
+
+
+	def train(self, train_num):
+		self.history = []
+		
+		for i in range(self.numSticks):
+			startArray = [1,2,3]
+			self.history.append(startArray)
+
+		for i in range(train_num):
+			ai1 = Ai()
+			ai2 = Ai()
+			winner = self.play(ai1, ai2)
+			print("winner is: ", getattr(winner, "name"))
+			for k, v in winner.array.items():
+				self.history[int(k)].append(v)
+			self.numSticks = self.startSticks
+		rematch = True
+		while rematch:
+
+			newPlayer = Player()
+			newPlayer.name = str(input("what's the player's name? "))
+			newWinner = self.play(newPlayer, Ai(), train = False)
+			print("winner is: ", getattr(newWinner, "name"))
+
+			rematch = str(input("would you like to rematch? "))
+			if rematch.upper() != "Y":
+				rematch = False
+			self.numSticks = self.startSticks
+
+
+	def play(self, player1, player2, train = True):
+		players = [player1, player2]
+		random.shuffle(players)
+		gen = cycle(players)
+		currentPlayer = next(gen)
+		while self.winner(currentPlayer) != True:
+			try:
+				if type(currentPlayer) == Player:
+					picks = int(input("How many sticks you want to pick? "))
+				else:
+					picks = self.history[self.numSticks - 1][random.randrange(len(self.history[self.numSticks - 1]))]
+					currentPlayer.array[str(self.numSticks - 1)] = picks
+				self.numSticks -= picks
+				if not train:
+					print("{} picked {} sticks, There are {} sticks left..".format(getattr(currentPlayer, "name"), picks ,self.numSticks))
+				currentPlayer = next(gen)
+			except ValueError as e:
+				print("Please choose between 1-3 sticks")
+		return currentPlayer
 
 
 
--- structure.py
+++ structure.py
@@ -1,5 +1,6 @@
 #python sets
 import random
+import sqlite3
 
 setA = set((10,30,13,10))
 setB = set((random.randrange(100) for x in range(100)))
@@ -24,3 +25,90 @@ print(p1 + p2)
 
 
 print(*zip(p1.coeff, p2.coeff))
+
+
+
+#metaclasses extends type
+class Base(): #libary
+	def foo(self):
+		return "foo"
+
+class Derived(Base): #user
+	def bar(self):
+		return self.foo()
+
+#derived class will fail if the base class methods is missing
+#how to change this? meta classes!!!
+
+
+class BaseMeta(type):
+	def __new__(klass, name, parent, body):
+		if not 'bar' in body:
+			print("function has no bar") 
+
+		print("BaseMeta().__new__ out", body)
+		return super().__new__(klass, name, parent, body)
+
+
+class Base1(metaclass = BaseMeta): #libary
+	def foo(self):
+		return self.bar()	#where does it get self.bar from?
+
+class Derived1(Base1): #user
+	def bar(self):
+		return "bar"	
+
+
+mybar = Derived1()
+mybar.bar()
+
+
+
+#power of decorators
+
+import time
+
+def timer(function):
+	def innnerWrapper(*args, **kwargs):
+		start = time.time()
+		value = function(*args, **kwargs)
+		total = time.time() - start
+
+		print("time took", total)
+
+		return value #has to return this, then this return value is carried to the return value of the function its decorating
+	return innnerWrapper
+
+@timer # have the timer function defined once, takes in any function and perform the task. used to add on more complex stuff along the projects
+def add(x,y):
+	return x + y
+@timer
+def hello(x,y,z):
+	for i in range(100):
+		x = x + y + z
+	return "hello" + str(x + y + z)
+@timer
+def flexAdd(*args, **kwargs):  #having *args and *key word arguments allows the flexible pass in of parameters
+	print(kwargs["myvalue"])
+	return sum(args)
+
+
+print("3 + 2 is : ", add(3,2))
+print("hello function: ", hello(1,34,5))
+print(flexAdd(3,5,6,5,6,7,77,4,3,2,5, myvalue="someday"))
+
+#generators
+#when gernerating a list, you dont want to wait for the entire list to finish generating before you can use it.
+#generator returns a value as a time as it generates more
+
+def compute():
+	for i in range(10):
+		time.sleep(0.5)
+		yield i
+for value in compute():
+	print(value)
+
+#another use of generators coroutines(generator) vs subroutines. coroutines + multi thread. coroutines gives permission and yeilds, and the mission is done by multiple threads
+#for webscrping, you want to be able to grab one page at a time using yeild, then give that one web page to one of the threads to handle
+
+#context manager/ resource allocation. match multple actions togther, such as opening a file must end with closing a file
